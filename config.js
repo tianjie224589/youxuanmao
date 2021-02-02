2046beddf53dd9e1db000fcdf5ed25c95d86cc9e
unknown <unknown@example.com>
2021-02-02 14:16:23 +0800

    2021-2-2
    14:16

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ var host = "http://www.yiyoucha.com/";
 var config = {
   host,
   getCode_url:host+"api/Weixin/getWxCode",
+  getClassify_url:host+"api/Weixin/getWxCode",
 }
 module.exports = config;
--- pages/classify/index.js
+++ pages/classify/index.js
@@ -1,93 +1,70 @@
-// pages/classify/index.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    mainActiveIndex: 0,
-    activeId: [],
-    max: 2,
-    items: [{ text: '分组 1' }, { text: '分组 2' }],
-    goodslist:[
-      {id:1,url:'https://img.yzcdn.cn/vant/apple-1.jpg'},
-      {id:2,url:'https://img.yzcdn.cn/vant/apple-2.jpg'}
-    ]
+    currentTab: '',
   },
-
-  onClickNav({ detail = {} }) {
-    this.setData({
-      mainActiveIndex: detail.index || 0,
-    });console.log(detail.index)
+  clickTab: function (e) {
+    let that = this;
+    that.setData({
+      currentTab: e.currentTarget.dataset.id,
+      id: e.currentTarget.dataset.id
+    })
   },
-
-  onClickItem({ detail = {} }) {
-    const { activeId } = this.data;
-
-    const index = activeId.indexOf(detail.id);
-    if (index > -1) {
-      activeId.splice(index, 1);
-    } else {
-      activeId.push(detail.id);
-    }
-    
-    this.setData({ activeId });
-  },
-
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    var that = this
+    if (options.share_id) {
+      wx.setStorageSync('share_id', options.share_id);
+      this.setData({
+        share_id: options.share_id
+      });
+    } else {
+      this.setData({
+        share_id: wx.getStorageSync('share_id')
+      });
+    }
+    wx.showLoading({ title: "加载中", mask: true });
+    wx.request({
+      url: 'https://qingyuan.6art.cn/goods/lists',
+      method: "post",
+      header: {
+        'Content-Type': 'application/json',
+      },
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          share:res.data.share,
+          title: res.data.categories,
+          currentTab:res.data.categories[0].tid
+        })
+      }
+    })
+    wx.request({
+      url: 'https://qingyuan.6art.cn/index/category',
+      method: "post",
+      header: {
+        'Content-Type': 'application/json',
+      },
+      success: function (res) {
+        wx.stopPullDownRefresh();
+        that.setData({
+          index_data: res.data.data
+        })
+        wx.hideLoading();
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow:function(){
+   
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    let userInfo = wx.getStorageSync('userInfo');
+    return {
+      title: this.data.share.desc,
+      imageUrl: this.data.share.shareImg,
+      path: '/pages/sort/sort?share_id=' + userInfo.uid,
+    }
   }
 })
